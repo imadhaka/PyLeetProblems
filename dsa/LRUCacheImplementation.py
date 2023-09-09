4bfc8f0d7a42cb4c444874e4d13201ddb9aebe9f
ankur <theankurdhaka@gmail.com>
2023-09-09 14:14:10 +0530

    Updated execution

--- dsa/LRUCacheImplementation.py
+++ dsa/LRUCacheImplementation.py
@@ -1,42 +1,62 @@
 #LRU Cache Implementation
+'''
+1. Get a ordered dictionary
+2. put -- if cache is full, remove the first key. Add the key to cache and move to end
+3. get -- if key is found, return key
+'''
+
 from collections import OrderedDict
+
+cache = OrderedDict()
 
 class LRUCacheImpl:
     def __init__(self, size):
         self.size = size
-        self.cache = OrderedDict()
 
     def put(self, key, value):
-        self.cache[key] = value
-        self.cache.move_to_end(key)
-        if len(self.cache) > self.size:
-            self.cache.popitem(last=False)
+        if len(cache) >= self.size:
+            cache.popitem(last=False)
+        cache[key] = value
+        cache.move_to_end(key)
 
     def get(self, key):
-        if key not in self.cache:
-            return -1
+        if key not in cache:
+            return 'key not found'
         else:
-            self.cache.move_to_end(key)
-            return self.cache[key]
+            cache.move_to_end(key)
+            return cache[key]
 
 
-cache = LRUCacheImpl(2)
+object = LRUCacheImpl(2)
 
-cache.put(1, 1)
-print(cache.cache)
-cache.put(2, 2)
-print(cache.cache)
-cache.get(1)
-print(cache.cache)
-cache.put(3, 3)
-print(cache.cache)
-cache.get(2)
-print(cache.cache)
-cache.put(4, 4)
-print(cache.cache)
-cache.get(1)
-print(cache.cache)
-cache.get(3)
-print(cache.cache)
-cache.get(4)
-print(cache.cache)
+object.put(1, 'A')
+print(cache)
+
+object.put(2, 'B')
+print(cache)
+
+res=object.get(1)
+print('value for 1 = ',res)
+print(cache)
+
+res=object.get(3)
+print('value for 3 = ',res)
+print(cache)
+
+object.put(3, 'C')
+print(cache)
+
+object.put(4, 'D')
+print(cache)
+
+res=object.get(2)
+print('value for 2 = ',res)
+print(cache)
+
+res=object.get(4)
+print('value for 4 = ',res)
+print(cache)
+
+res=object.get(3)
+print('value for 3 = ',res)
+print(cache)
