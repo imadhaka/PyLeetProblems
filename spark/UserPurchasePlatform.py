c929a8c7200779171222f01898617d3aa7d04e5e
ankur <theankurdhaka@gmail.com>
2024-01-06 10:07:35 +0530

    Updated UserPurchasePlatform.py

--- spark/UserPurchasePlatform.py
+++ spark/UserPurchasePlatform.py
@@ -71,33 +71,53 @@ class UserPurchase:
         inputDf.createOrReplaceTempView('table')
 
         query = """with cte as (
-                    select user_id, spend_date,
-                    count(distinct platform) as platformCount
-                    from table group by user_id, spend_date),
-                    
-                    cte2 as (
-                    select M1.spend_date,
-                    if(M2.platformCount > 1, 'both', M1.platform) as platform,
-                    M1.amount, M1.user_id 
-                    from table M1 join cte M2
-                    on M1.user_id = M2.user_id 
-                    and M1.spend_date = M2.spend_date) 
-                    
-                    select spend_date, platform,
-                    sum(amount) as total_amount,
-                    count(distinct user_id) as total_users
-                    from cte2 group by spend_date, platform
-                    order by spend_date, total_users"""
+                            select user_id, spend_date,
+                            count(distinct platform) as platformCount
+                            from table group by user_id, spend_date),
+
+                            cte2 as (
+                            select M1.spend_date,
+                            if(M2.platformCount > 1, 'both', M1.platform) as platform,
+                            M1.amount, M1.user_id 
+                            from table M1 join cte M2
+                            on M1.user_id = M2.user_id 
+                            and M1.spend_date = M2.spend_date), 
+                            
+                            cte3 as (
+                            select distinct spend_date, X.platform 
+                            from table join 
+                            (SELECT 'desktop' AS platform UNION
+                            SELECT 'mobile' AS platform UNION
+                            SELECT 'both' AS platform) X )
+
+                            select A.spend_date, A.platform,
+                            coalesce(sum(amount), 0) as total_amount,
+                            coalesce(count(distinct user_id), 0) as total_users
+                            from cte3 A left join cte2 B
+                            on A.platform = B.platform 
+                            and A.spend_date = B.spend_date
+                            group by A.spend_date, A.platform
+                            order by A.spend_date, A.platform desc"""
 
         return spark.sql(query)
 
     def totalUserAmount(self, inputDf):
-        groupDf = inputDf.groupBy('user_id', 'spend_date').agg(count_distinct('platform').alias('platformCount'))
+        platformData = [('desktop',), ('mobile',), ('both',)]
+        schema = ['platform']
+        platformCntdf = inputDf.groupBy('user_id', 'spend_date').agg(count_distinct('platform').alias('platformCount'))
 
-        return groupDf.alias('M2').join(inputDf.alias('M1'), (groupDf['user_id']==inputDf['user_id']) & (groupDf['spend_date']==inputDf['spend_date']), 'inner')\
+        bothDf = spark.createDataFrame(platformData, schema)
+        bothDf = bothDf.alias('M1').crossJoin(inputDf).select('spend_date', 'M1.platform').distinct()
+
+        joinDf = platformCntdf.alias('M2').join(inputDf.alias('M1'), (platformCntdf['user_id']==inputDf['user_id']) & (platformCntdf['spend_date']==inputDf['spend_date']), 'inner')\
             .withColumn('platform', when(col('M2.platformCount').__gt__(1), lit('both')).otherwise(col('M1.platform')))\
-            .groupBy('M1.spend_date', 'platform').agg(sum('M1.amount').alias('total_amount'), count_distinct('M1.user_id').alias('total_users'))\
-                        .orderBy('M1.spend_date', 'total_users')
+            .select(col('M1.spend_date').alias('spend_date'), 'platform', 'amount', col('M1.user_id').alias('user_id'))
+
+        joinDf = bothDf.alias('A').join(joinDf.alias('B'), (bothDf['platform']==joinDf['platform']) & (bothDf['spend_date']==joinDf['spend_date']), 'left')\
+                .groupBy('A.spend_date', 'A.platform').agg(coalesce(sum('B.amount'), lit(0)).alias('total_amount'), count_distinct('B.user_id').alias('total_users'))\
+                        .orderBy('A.spend_date', desc('A.platform'))
+
+        return joinDf
 
 ob = UserPurchase()
 inputDf = ob.createData()
