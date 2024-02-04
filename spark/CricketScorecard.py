5aea6ac92bc26620f803e540f3e0d9beb41e2e27
ankur <theankurdhaka@gmail.com>
2024-02-04 14:13:55 +0530

    Updated CricketScorecard.py

--- spark/CricketScorecard.py
+++ spark/CricketScorecard.py
@@ -18,7 +18,7 @@ Few things to remember
 
 import os, sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, row_number, ceil, min, max, lit, sum, lag, least
+from pyspark.sql.functions import col, row_number, ceil, min, max, lit, sum, lag, least, coalesce, when
 from pyspark.sql.window import Window
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
@@ -31,10 +31,16 @@ class CricketScorecard:
 
     def getOverRuns(self, inputDf):
         inputDf.createOrReplaceTempView('table')
-        query = """with legal as (
-                select int(delivery_no) as delivery_no, runs_scored, delivery_type,
-                ceil(row_number() over(order by int(delivery_no)) / 6) as over
+        query = """with input as (
+                select int(delivery_no) as delivery_no,
+                runs_scored, delivery_type
                 from table
+                ),
+                
+                legal as (
+                select delivery_no, runs_scored,
+                ceil(row_number() over(order by delivery_no) / 6) as over
+                from input
                 where delivery_type = 'legal'),
                 
                 overDelivery as (
@@ -51,17 +57,30 @@ class CricketScorecard:
                     ) M1
                 ),
                 
-                notLegal as (
+                extras as (
                 select o.over,
-                int(t.runs_scored) + 1 as runs_scored
-                from table t join overDelivery o
+                int(if(t.runs_scored = 'W', 0, t.runs_scored)) + 1 as runs_scored
+                from input t join overDelivery o
                 on t.delivery_no BETWEEN least(o.first_ball, o.prev_over_last_del) AND o.last_ball
                 where t.delivery_type != 'legal'
-                ) 
+                ),
+                
+                lastDelivery as (
+                select
+                if(l.max_delivery_no = 6,  max_over + 1, max_over) as over,
+                sum(coalesce(int(t.runs_scored), 0)) + 1 as runs_scored
+                from input t JOIN (SELECT MAX(last_ball) AS max_delivery_no, max(over) as max_over FROM overDelivery) l
+                ON t.delivery_no > l.max_delivery_no
+                group by l.max_over, l.max_delivery_no
+                )
                 
                 select over, sum(runs_scored) as runs_scored
-                from (select over, runs_scored from legal union all select over, runs_scored from notLegal) M1
+                from (select over, runs_scored from legal union all 
+                        select over, runs_scored from extras union all 
+                        select over, runs_scored from lastDelivery
+                    ) M1
                 group by over order by over
+                
                 """
         return spark.sql(query)
 
@@ -77,9 +96,16 @@ class CricketScorecard:
         # Calculate overs for extra deliveries
         extrasDf = cricket_data.filter("delivery_type != 'legal'").alias('A')\
                                 .join(deliveryDf.alias('B'), col('A.delivery_no').between(least(col('B.first_ball'), col('prev_over_last_del')), col('B.last_ball')))\
-                                .select('B.over', (col("A.runs_scored").cast("int") + 1).alias("runs_scored"))
+                                .select('B.over', (coalesce(col("A.runs_scored").cast("int"), lit(0)) + 1).alias("runs_scored"))
 
-        resultDf = oversDf.select('over', 'runs_scored').unionAll(extrasDf)\
+        # Calculate runs if innings last ball is extra
+        maxOverDf = deliveryDf.agg(max('last_ball').alias('max_delivery'), max('over').alias('max_over'))
+        lastDeliveryDf = cricket_data.join(maxOverDf, col('delivery_no') > col('max_delivery'))\
+                        .groupBy('max_delivery', 'max_over').agg((sum(coalesce(col("runs_scored").cast("int"), lit(0)))+1).alias('runs_scored'))\
+                        .withColumn('over', when(col('max_delivery').__eq__(6), col('max_over')+1).otherwise(col('max_over')))\
+                        .select('over', 'runs_scored')
+
+        resultDf = oversDf.select('over', 'runs_scored').unionAll(extrasDf).unionAll(lastDeliveryDf)\
                         .groupBy('over').agg(sum('runs_scored').alias('runs_scored'))\
                         .orderBy('over')
 
