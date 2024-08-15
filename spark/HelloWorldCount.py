0e9d26a62d3aa370e21e2cdea538b0562f438975
ankur <theankurdhaka@gmail.com>
2024-08-15 16:16:38 +0530

    Added HelloWorldCount.py

--- spark/HelloWorldCount.py
+++ spark/HelloWorldCount.py
@@ -6,22 +6,34 @@ os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 spark = SparkSession.builder.master('local[*]').getOrCreate()
 
-class HelloWorld:
+class HelloWorldCount:
     def createData(self):
         return spark.read.text('C:/Users/thean/PycharmProjects/PyLeetProblems/data/helloworld.txt')
 
     def countWords(self, inputDf):
-        # Use regexp_extract to find occurrences of "Hello World"
+        # Replace each occurrence of "Hello World" with an empty string and calculate the difference in length
         pattern = "Hello World"
         count_df = inputDf.withColumn("num_hello_world", (length(col("value")) - length(regexp_replace(col("value"), pattern, ""))) / length(lit(pattern)))
-        count_df.show()
+        #count_df.show()
 
         # Sum up all the counts to get the total occurrences of "Hello World"
         total_count = count_df.agg({"num_hello_world": "sum"}).collect()[0][0]
         print(f"The phrase 'Hello World' occurs {total_count} times.")
 
-ob = HelloWorld()
-df = ob.createData()
-df.show(truncate=False)
+    def countWordSql(self, inputDf):
+        inputDf.createOrReplaceTempView('table')
+        pattern_len = len("Hello World")
+        query = f'''
+            with cte as 
+            (select (length(value) - length(regexp_replace(value, 'Hello World', ''))) / {pattern_len} as num from table)
+            select sum(num) as Hello_world_count from cte
+        '''
+        return spark.sql(query)
 
-count = ob.countWords(df)
+ob = HelloWorldCount()
+df = ob.createData()
+#df.show(truncate=False)
+
+ob.countWords(df)
+result = ob.countWordSql(df)
+result.show(truncate=False)
